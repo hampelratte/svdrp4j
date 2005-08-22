2eee33a59339518c428866142d09375e3ea98f3f
hampelratte <hampelratte>
2005-08-22 10:55:55 +0000

    better names: major, minor, revision instead of version, major, minor

--- src/de/hampelratte/svdrp/VDRVersion.java
+++ src/de/hampelratte/svdrp/VDRVersion.java
@@ -10,57 +10,49 @@ import java.util.regex.Pattern;
  */
 public class VDRVersion {
 
-  int version = 0;
+    int major = 0;
 
-  int major = 0;
+    int minor = 0;
 
-  int minor = 0;
+    int revision = 0;
 
+    public VDRVersion(String versionString) {
+        Pattern pattern = Pattern
+                .compile("((?:\\d)+)\\.((?:\\d)+)\\.((?:\\d)+)");
+        Matcher m = pattern.matcher(versionString);
 
-  public VDRVersion(String versionString) {
-    Pattern pattern = Pattern
-        .compile("((?:\\d)+)\\.((?:\\d)+)\\.((?:\\d)+)");
-    Matcher m = pattern.matcher(versionString);
-
-    if (m.matches()) {
-      version = Integer.parseInt(m.group(1));
-      major = Integer.parseInt(m.group(2));
-      minor = Integer.parseInt(m.group(3));
+        if (m.matches()) {
+            major = Integer.parseInt(m.group(1));
+            minor = Integer.parseInt(m.group(2));
+            revision = Integer.parseInt(m.group(3));
+        }
     }
-  }
 
+    public int getMinor() {
+        return minor;
+    }
 
-  public int getMajor() {
-    return major;
-  }
+    public void setMinor(int major) {
+        this.minor = major;
+    }
 
+    public int getRevision() {
+        return revision;
+    }
 
-  public void setMajor(int major) {
-    this.major = major;
-  }
+    public void setRevision(int minor) {
+        this.revision = minor;
+    }
 
+    public int getMajor() {
+        return major;
+    }
 
-  public int getMinor() {
-    return minor;
-  }
+    public void setMajor(int version) {
+        this.major = version;
+    }
 
-
-  public void setMinor(int minor) {
-    this.minor = minor;
-  }
-
-
-  public int getVersion() {
-    return version;
-  }
-
-
-  public void setVersion(int version) {
-    this.version = version;
-  }
-  
-  
-  public String toString() {
-    return version+"."+major+"."+minor;
-  }
+    public String toString() {
+        return major + "." + minor + "." + revision;
+    }
 }
