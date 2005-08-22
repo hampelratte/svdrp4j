93495f2606868dae8bd30d1e5d04dd3fc3f0e37e
hampelratte <hampelratte>
2005-08-22 11:24:20 +0000

    connection now supports timeouts. still has to be integrated into settings/settingspanel

--- src/de/hampelratte/svdrp/Connection.java
+++ src/de/hampelratte/svdrp/Connection.java
@@ -4,78 +4,91 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 import de.hampelratte.svdrp.commands.QUIT;
-import de.hampelratte.svdrp.responses.AccessDenied;
-import de.hampelratte.svdrp.responses.R214;
-import de.hampelratte.svdrp.responses.R215;
-import de.hampelratte.svdrp.responses.R220;
-import de.hampelratte.svdrp.responses.R221;
-import de.hampelratte.svdrp.responses.R250;
-import de.hampelratte.svdrp.responses.R354;
-import de.hampelratte.svdrp.responses.R451;
-import de.hampelratte.svdrp.responses.R500;
-import de.hampelratte.svdrp.responses.R501;
-import de.hampelratte.svdrp.responses.R502;
-import de.hampelratte.svdrp.responses.R550;
-import de.hampelratte.svdrp.responses.R554;
-
+import de.hampelratte.svdrp.responses.*;
 
 /**
- * The connection to VDR. I recommend to use only one connection at a time, 
+ * The connection to VDR. I recommend to use only one connection at a time,
  * since VDR just accepts one client.
  * 
  * @author <a href="mailto:henrik.niehaus@gmx.de">Henrik Niehaus</a>
  */
 public class Connection {
-  	/**
-  	 * The socket used to talk to VDR
-  	 */
+    /**
+     * The socket used to talk to VDR
+     */
     private Socket socket;
+
     /**
      * The PrintStream to send commands to VDR
      */
     private PrintStream out;
+
     /**
      * The BufferedReader to read responses from VDR
      */
     private BufferedReader in;
-    
 
     private static VDRVersion version;
-    
+
     public static boolean DEBUG = false;
-    
+
     /**
-     * Creates a new new connection to host:port
-     * @param host The host name or IP-address of the VDR
-     * @param port The port of the SVDRP-server. Default is 2001
+     * Creates a new new connection to host:port with timeout
+     * 
+     * @param host
+     *            The host name or IP-address of the VDR
+     * @param port
+     *            The port of the SVDRP-server. Default is 2001
+     * @param timeout
+     *            The timeout for this connection
      * @throws UnknownHostException
      * @throws IOException
      */
-    public Connection(String host, int port) throws UnknownHostException, IOException {
-        socket = new Socket(host, port);
+    public Connection(String host, int port, int timeout)
+            throws UnknownHostException, IOException {
+        socket = new Socket();
+        InetSocketAddress sa = new InetSocketAddress(host, port);
+        socket.connect(sa, timeout);
         out = new PrintStream(socket.getOutputStream());
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        
+
         // read the welcome message
         Response res = readResponse();
-        if(res.getCode() == 220) {
-          String msg = res.getMessage();
-          int start = msg.indexOf("VideoDiskRecorder") + 17;
-          int stop = msg.indexOf(";");
-          String vers = msg.substring(start, stop).trim(); 
-          version = new VDRVersion(vers);
+        if (res.getCode() == 220) {
+            String msg = res.getMessage();
+            int start = msg.indexOf("VideoDiskRecorder") + 17;
+            int stop = msg.indexOf(";");
+            String vers = msg.substring(start, stop).trim();
+            version = new VDRVersion(vers);
         } else {
-          throw new IOException(res.getMessage());
+            throw new IOException(res.getMessage());
         }
     }
-    
+
+    /**
+     * 
+     * @param host
+     *            The host name or IP-address of the VDR
+     * @param port
+     *            The port of the SVDRP-server. Default is 2001
+     * @throws UnknownHostException
+     * @throws IOException
+     */
+    public Connection(String host, int port) throws UnknownHostException,
+            IOException {
+        this(host, port, 500);
+    }
+
     /**
      * Sends a command to VDR and returns the response from VDR.
-     * @param cmd The Command, which should be sent to VDR
+     * 
+     * @param cmd
+     *            The Command, which should be sent to VDR
      * @return A Response object
      * @throws IOException
      * @see de.hampelratte.svdrp.Command
@@ -84,31 +97,30 @@ public class Connection {
     public synchronized Response send(Command cmd) throws IOException {
         // send the command
         out.println(cmd.getCommand());
-        if(DEBUG)
-          System.out.println("-->"+cmd.getCommand());
-        
+        if (DEBUG)
+            System.out.println("-->" + cmd.getCommand());
+
         // read the response
         Response response = readResponse();
-        if(DEBUG)
-          System.out.println("<--"+response.getCode()+" "+response.getMessage());
-        
+        if (DEBUG)
+            System.out.println("<--" + response.getCode() + " "
+                    + response.getMessage());
+
         /*
-        // TODO parse the response and create an adequate object
-        if(cmd instanceof LSTE) {
-          List epgdata = EPGParser.parse(response.getMessage());
-          for (Iterator iter = epgdata.iterator(); iter.hasNext();) {
-            EPGEntry element = (EPGEntry) iter.next();
-            System.out.println(element);
-          }
-        }*/
-        
-        // return the response 
+         * // TODO parse the response and create an adequate object if(cmd
+         * instanceof LSTE) { List epgdata =
+         * EPGParser.parse(response.getMessage()); for (Iterator iter =
+         * epgdata.iterator(); iter.hasNext();) { EPGEntry element = (EPGEntry)
+         * iter.next(); System.out.println(element); } }
+         */
+
+        // return the response
         return response;
     }
-    
+
     /**
-     * Reads the response for a sent command.
-     * Used by send()
+     * Reads the response for a sent command. Used by send()
+     * 
      * @return A Response object
      * @see Response
      */
@@ -122,20 +134,20 @@ public class Connection {
                 char fourthChar = line.charAt(3);
                 int code = -1;
                 try {
-                  code = Integer.parseInt(line.substring(0, 3));
-                  line = line.substring(4);
+                    code = Integer.parseInt(line.substring(0, 3));
+                    line = line.substring(4);
                 } catch (Exception e) {
-                  code = -1;
+                    code = -1;
                 }
-                
+
                 if (fourthChar != '-') {
                     running = false;
                     msg.append(line);
                     msg.append("\n");
                     switch (code) {
                     case -1:
-                      	response = new AccessDenied(line);
-                      	break;
+                        response = new AccessDenied(line);
+                        break;
                     case 214:
                         response = new R214(msg.toString());
                         break;
@@ -185,21 +197,23 @@ public class Connection {
         }
         return response;
     }
-    
+
     /**
-     * Closes the connection to VDR.
-     * After closing a connection you have to create a new connection to talk to VDR.
+     * Closes the connection to VDR. After closing a connection you have to
+     * create a new connection to talk to VDR.
+     * 
      * @throws IOException
      */
     public void close() throws IOException {
         send(new QUIT());
     }
-    
+
     /**
      * Returns the version of the VDR we are talking to.
+     * 
      * @return The version of the VDR we are talking to.
      */
     public static VDRVersion getVersion() {
-      return version;
+        return version;
     }
 }
