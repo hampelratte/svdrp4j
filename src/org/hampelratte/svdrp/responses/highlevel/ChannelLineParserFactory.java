51f1b1efd93ddbc362128f590abbbf727e3cace6
hampelratte <hampelratte>
2008-07-14 17:16:39 +0000

    changed dvb channel detection

--- src/org/hampelratte/svdrp/responses/highlevel/ChannelLineParserFactory.java
+++ src/org/hampelratte/svdrp/responses/highlevel/ChannelLineParserFactory.java
@@ -2,31 +2,32 @@ package org.hampelratte.svdrp.responses.highlevel;
 
 
 
+
 public class ChannelLineParserFactory {
-    
-    
 	
 	private static DVBChannelLineParser dvb;
 	
 	//private static IPTVChannelLineParser iptv;
 	
-	
 	public static ChannelLineParser createChannelParser(String chanConfLine) throws Exception {
 		/* TODO enable if IPTV is fully supported if(chanConfLine.toLowerCase().contains("iptv")) {
 			if(iptv == null) iptv = new IPTVChannelLineParser();
 			return iptv;
-		} else*/ if(isDvbChannel(chanConfLine)) { 
+		} else*/ 
+	    if(isDvbChannel(chanConfLine)) { 
 			if(dvb == null) dvb = new DVBChannelLineParser();
 			return dvb;
-		} else {
+	    } else {
 			throw new Exception("Unknown format for channels.conf lines: " + chanConfLine);
 		}
 	}
 
-    private static DVBChannel nullChannel = new DVBChannel();
 	private static boolean isDvbChannel(String chanConfLine) {
 	    String[] parts = chanConfLine.split(":");
-        DVBChannelLineParser.parseParameters(nullChannel, parts[2]);
-        return true;
+        if (parts.length >= 4 && (parts[3].startsWith("S") || parts[3].startsWith("C") || parts[3].startsWith("T"))) {
+            return true;
+        }
+        
+        return false;
 	}
 }
