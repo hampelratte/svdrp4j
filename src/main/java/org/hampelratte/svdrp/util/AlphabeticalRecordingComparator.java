b5af54d8047cdac6081b357169bebe1047f2555e
hampelratte <hampelratte>
2010-06-13 17:30:19 +0000

    use the display title instead of the raw title for comparison

--- src/main/java/org/hampelratte/svdrp/util/AlphabeticalRecordingComparator.java
+++ src/main/java/org/hampelratte/svdrp/util/AlphabeticalRecordingComparator.java
@@ -6,16 +6,8 @@ import org.hampelratte.svdrp.responses.highlevel.Recording;
 
 public class AlphabeticalRecordingComparator implements Comparator<Recording> {
     public int compare(Recording r1, Recording r2) {
-        String title1 = r1.getTitle();
-        String title2 = r2.getTitle();
-
-        if (title1.charAt(0) == '%' || title1.charAt(0) == '@') {
-            title1 = title1.substring(1);
-        }
-        if (title2.charAt(0) == '%' || title2.charAt(0) == '@') {
-            title2 = title2.substring(1);
-        }
-
+        String title1 = r1.getDisplayTitle();
+        String title2 = r2.getDisplayTitle();
         return title1.toLowerCase().compareTo(title2.toLowerCase());
     }
 }
