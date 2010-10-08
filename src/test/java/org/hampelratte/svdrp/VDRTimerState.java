d6f16d01353bbf148c0ab8e6245647632c0269fa
hampelratte <hampelratte>
2010-10-08 17:37:01 +0000

    unit test for the timer state

---
+++ src/test/java/org/hampelratte/svdrp/VDRTimerState.java
@@ -0,0 +1,66 @@
+package org.hampelratte.svdrp;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.hampelratte.svdrp.responses.highlevel.VDRTimer;
+import org.junit.Test;
+
+public class VDRTimerState {
+    
+    private VDRTimer timer = new VDRTimer();
+    
+    @Test
+    public void testChangeState() {
+        timer.changeStateTo(VDRTimer.ACTIVE, true);
+        timer.changeStateTo(VDRTimer.RECORDING, true);
+        timer.changeStateTo(VDRTimer.VPS, true);
+        
+        assertTrue(timer.hasState(VDRTimer.ACTIVE));
+        assertTrue(timer.hasState(VDRTimer.RECORDING));
+        assertTrue(timer.hasState(VDRTimer.VPS));
+        
+        timer.changeStateTo(VDRTimer.RECORDING, false);
+        
+        assertTrue(timer.hasState(VDRTimer.ACTIVE));
+        assertFalse(timer.hasState(VDRTimer.RECORDING));
+        assertTrue(timer.hasState(VDRTimer.VPS));
+        
+        timer.changeStateTo(VDRTimer.ACTIVE, false);
+        timer.changeStateTo(VDRTimer.VPS, false);
+        timer.changeStateTo(VDRTimer.INSTANT_TIMER, true);
+        
+        assertFalse(timer.hasState(VDRTimer.ACTIVE));
+        assertFalse(timer.hasState(VDRTimer.RECORDING));
+        assertFalse(timer.hasState(VDRTimer.VPS));
+        assertTrue(timer.hasState(VDRTimer.INSTANT_TIMER));
+    }
+    
+    @Test
+    public void testSetState() {
+        timer.setState(VDRTimer.ACTIVE | VDRTimer.VPS);
+        
+        assertTrue(timer.hasState(VDRTimer.ACTIVE));
+        assertFalse(timer.hasState(VDRTimer.RECORDING));
+        assertTrue(timer.hasState(VDRTimer.VPS));
+        assertFalse(timer.hasState(VDRTimer.INSTANT_TIMER));
+    }
+    
+    @Test
+    public void testIsActive() {
+        timer.setState(VDRTimer.ACTIVE);
+        assertTrue(timer.isActive());
+    }
+    
+    @Test
+    public void testIsRecording() {
+        timer.setState(VDRTimer.ACTIVE | VDRTimer.RECORDING);
+        assertTrue(timer.isRecording());
+        
+        timer.setState(VDRTimer.ACTIVE);
+        assertFalse(timer.isRecording());
+        
+        timer.setState(VDRTimer.RECORDING);
+        assertFalse(timer.isRecording());
+    }
+}
