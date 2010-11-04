dc50ff2f03755ba2a712c6a9f6c3b54edc031007
hampelratte <hampelratte>
2010-11-04 14:37:51 +0000

    moved state tests to VDRTimerTest

--- src/test/java/org/hampelratte/svdrp/responses/highlevel/VDRTimerTest.java
+++ src/test/java/org/hampelratte/svdrp/responses/highlevel/VDRTimerTest.java
@@ -150,6 +150,70 @@ public class VDRTimerTest  {
         assertEquals("2010-11-04", timer.getDayString());
     }
     
+    @Test
+    public void testChangeState() {
+        timer.changeStateTo(VDRTimer.ACTIVE, true);
+        timer.changeStateTo(VDRTimer.RECORDING, true);
+        timer.changeStateTo(VDRTimer.VPS, true);
+        
+        assertTrue(timer.hasState(VDRTimer.ACTIVE));
+        assertTrue(timer.hasState(VDRTimer.RECORDING));
+        assertTrue(timer.hasState(VDRTimer.VPS));
+        
+        timer.changeStateTo(VDRTimer.RECORDING, false);
+        
+        assertTrue(timer.hasState(VDRTimer.ACTIVE));
+        assertFalse(timer.hasState(VDRTimer.RECORDING));
+        assertTrue(timer.hasState(VDRTimer.VPS));
+        
+        timer.changeStateTo(VDRTimer.ACTIVE, false);
+        timer.changeStateTo(VDRTimer.VPS, false);
+        timer.changeStateTo(VDRTimer.INSTANT_TIMER, true);
+        
+        assertFalse(timer.hasState(VDRTimer.ACTIVE));
+        assertFalse(timer.hasState(VDRTimer.RECORDING));
+        assertFalse(timer.hasState(VDRTimer.VPS));
+        assertTrue(timer.hasState(VDRTimer.INSTANT_TIMER));
+    }
+    
+    @Test
+    public void testSetState() {
+        timer.setState(VDRTimer.ACTIVE | VDRTimer.VPS);
+        
+        assertTrue(timer.hasState(VDRTimer.ACTIVE));
+        assertFalse(timer.hasState(VDRTimer.RECORDING));
+        assertTrue(timer.hasState(VDRTimer.VPS));
+        assertFalse(timer.hasState(VDRTimer.INSTANT_TIMER));
+    }
+    
+    @Test
+    public void testIsActive() {
+        timer.setState(VDRTimer.ACTIVE);
+        assertTrue(timer.isActive());
+    }
+    
+    @Test
+    public void testIsRecording() {
+        timer.setState(VDRTimer.ACTIVE | VDRTimer.RECORDING);
+        assertTrue(timer.isRecording());
+        
+        timer.setState(VDRTimer.ACTIVE);
+        assertFalse(timer.isRecording());
+        
+        timer.setState(VDRTimer.RECORDING);
+        assertFalse(timer.isRecording());
+        
+        timer.setState(VDRTimer.ACTIVE);
+        assertFalse(timer.isRecording());
+        Calendar start = Calendar.getInstance();
+        start.add(Calendar.HOUR, -1);
+        timer.setStartTime(start);
+        Calendar stop = Calendar.getInstance();
+        stop.add(Calendar.HOUR, 1);
+        timer.setEndTime(stop);
+        assertTrue(timer.isRecording());
+    }
+    
     @AfterClass 
     public static void shutdownServer() throws IOException, InterruptedException {
         server.shutdown();
