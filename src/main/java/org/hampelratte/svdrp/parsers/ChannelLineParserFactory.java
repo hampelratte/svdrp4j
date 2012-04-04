ddbd481d992b3bcdc8ab37f18e49424c3423713b
Henrik Niehaus <henrik.niehaus@gmx.de>
2012-04-04 14:04:54 +0200

    added the keyword "analog" for the pvrinput parser

--- src/main/java/org/hampelratte/svdrp/parsers/ChannelLineParserFactory.java
+++ src/main/java/org/hampelratte/svdrp/parsers/ChannelLineParserFactory.java
@@ -7,11 +7,11 @@
  * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 3. Neither the name of the project (Lazy Bones) nor the names of its 
- *    contributors may be used to endorse or promote products derived from this 
+ * 3. Neither the name of the project (Lazy Bones) nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -68,7 +68,8 @@ public class ChannelLineParserFactory {
     }
 
     private static boolean isPvrInputChannel(String chanConfLine) {
-        return chanConfLine.toLowerCase().contains("pvrinput") || chanConfLine.contains("w_pvrscan");
+        return chanConfLine.toLowerCase().contains("pvrinput") || chanConfLine.toLowerCase().contains("w_pvrscan")
+                || chanConfLine.toLowerCase().contains("analog");
     }
 
     private static boolean isDvbChannel(String chanConfLine) {
