07957f153524e8bd633282cdbd529951e8175422
Henrik Niehaus <henrik.niehaus@gmx.de>
2013-07-22 15:50:50 +0200

    Use Integer.vauleOf instead of constructor

--- src/test/java/org/hampelratte/svdrp/parser/DVBChannelLineParserTest.java
+++ src/test/java/org/hampelratte/svdrp/parser/DVBChannelLineParserTest.java
@@ -103,7 +103,7 @@ public class DVBChannelLineParserTest {
 
     @Test
     public void testConditionalAccess() {
-        assertEquals(new Integer(0), chan.getConditionalAccess().get(0));
+        assertEquals(Integer.valueOf(0), chan.getConditionalAccess().get(0));
 
         String channelData = "1 Sky Sport 1,Sport1;SKY:12031:HC34M2S0:S19.2E:27500:2047=2:2048=deu@3,2049=deu@3:32:1702,1722,1833,1834,9C4,9C7,9AF,98C,1861:221:133:4:0";
         DVBChannel chan = (DVBChannel) parser.parse(channelData);
@@ -124,7 +124,7 @@ public class DVBChannelLineParserTest {
     public void testReelboxConditionalAccess() {
         String channelData = "1 SF 1 HD;Schweizer Fernsehen:10971:HC23M5O35S1:S13.0E:29700:502=27:503=deu@3,504=eng@3;505=mul@106:507:S11:17201:318:12300:0";
         DVBChannel chan = (DVBChannel) parser.parse(channelData);
-        assertEquals(new Integer(17), chan.getConditionalAccess().get(0));
+        assertEquals(Integer.valueOf(17), chan.getConditionalAccess().get(0));
     }
 
     @Test
