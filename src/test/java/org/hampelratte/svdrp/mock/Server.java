4136a1605f9047d2fab9dd988e9870a3bc5f4ef9
Henrik Niehaus <henrik.niehaus@gmx.de>
2013-07-23 12:53:29 +0200

    Ignore groups in channels.conf

--- src/test/java/org/hampelratte/svdrp/mock/Server.java
+++ src/test/java/org/hampelratte/svdrp/mock/Server.java
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) Henrik Niehaus
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
@@ -13,7 +13,7 @@
  * 3. Neither the name of the project (Lazy Bones) nor the names of its
  *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -106,6 +106,12 @@ public class Server implements Runnable {
         int channelNumber = 0;
         while(st.hasMoreElements()) {
             String line = st.nextToken();
+
+            // ignore groups
+            if (line.startsWith(":")) {
+                continue;
+            }
+
             char delim = st.hasMoreElements() ? '-' : ' ';
             channels += "250" + delim + Integer.toString(++channelNumber) + ' ' + line;
             if(st.hasMoreElements()) {
