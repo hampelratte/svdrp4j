7ab43665737ec1d0418a81e190bbea9d1fa0b05f
Henrik Niehaus <henrik.niehaus@gmx.de>
2013-07-23 11:19:13 +0200

    Refactored some methods

--- src/test/java/org/hampelratte/svdrp/mock/TimerManager.java
+++ src/test/java/org/hampelratte/svdrp/mock/TimerManager.java
@@ -6,12 +6,8 @@ import java.util.List;
 
 import org.hampelratte.svdrp.parsers.TimerParser;
 import org.hampelratte.svdrp.responses.highlevel.Timer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TimerManager {
-    private static transient Logger logger = LoggerFactory.getLogger(TimerManager.class);
-
     private List<Timer> timers;
 
     public void parseData(String data) {
@@ -41,38 +37,27 @@ public class TimerManager {
     }
 
     public int addTimer(Timer timer) {
-        for (int i = 0; i < timers.size(); i++) {
-            Timer existing = timers.get(i);
-            int id = i + 1;
-            if (existing.getID() != id) {
-                logger.debug("Existing ID: {}, Index: {}", existing.getID(), id);
-                timer.setID(id);
-                timers.add(id - 1, timer);
-                return id;
-            }
-        }
-
         timers.add(timer);
         int id = timers.size();
         timer.setID(id);
         return id;
     }
 
-    public boolean removeTimer(Timer timer) {
-        return timers.remove(timer);
-    }
-
     public boolean removeTimer(int id) {
         Timer timer = getTimer(id);
-        if (timer != null) {
-            boolean removed = timers.remove(timer);
-            if (removed) {
-                renumberTimers();
-            }
-            return removed;
-        } else {
-            return false;
+        return removeTimer(timer);
+    }
+
+    public boolean removeTimer(Timer timer) {
+        return removeAndRenumber(timer);
+    }
+
+    private boolean removeAndRenumber(Timer timer) {
+        boolean removed = timers.remove(timer);
+        if (removed) {
+            renumberTimers();
         }
+        return removed;
     }
 
     private void renumberTimers() {
