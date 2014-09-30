234dc16aaee9eb28136e2a9c46544e9e3c121f8b
Henrik Niehaus <henrik.niehaus@gmx.de>
2014-09-30 15:00:47 +0200

    Reactivated test for the VDR facade and mocked all server communication with mockito.

--- src/test/java/org/hampelratte/svdrp/VdrFacadeTest.java
+++ src/test/java/org/hampelratte/svdrp/VdrFacadeTest.java
@@ -28,135 +28,262 @@
  */
 package org.hampelratte.svdrp;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+import org.hampelratte.svdrp.commands.DELT;
+import org.hampelratte.svdrp.commands.LSTC;
+import org.hampelratte.svdrp.commands.LSTE;
+import org.hampelratte.svdrp.commands.LSTR;
+import org.hampelratte.svdrp.commands.LSTT;
+import org.hampelratte.svdrp.responses.AccessDenied;
+import org.hampelratte.svdrp.responses.R215;
+import org.hampelratte.svdrp.responses.R250;
+import org.hampelratte.svdrp.responses.R550;
+import org.hampelratte.svdrp.responses.highlevel.Channel;
+import org.hampelratte.svdrp.responses.highlevel.EPGEntry;
+import org.hampelratte.svdrp.responses.highlevel.Genre;
+import org.hampelratte.svdrp.responses.highlevel.Recording;
+import org.hampelratte.svdrp.responses.highlevel.Stream;
+import org.hampelratte.svdrp.responses.highlevel.Timer;
+import org.hampelratte.svdrp.util.IOUtil;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 
 
 public class VdrFacadeTest {
 
-    // private static Server server;
-    //
-    // private static VDR vdr;
-    //
-    // @BeforeClass
-    // public static void startMockServer() throws IOException, InterruptedException {
-    // server = new Server();
-    // server.loadWelcome("welcome-1.6.0_2-utf_8.txt");
-    // server.loadRecordings("lstr.txt");
-    // server.loadTimers("lstt.txt");
-    // new Thread(server).start();
-    //
-    // // wait for the server
-    // Thread.sleep(1000);
-    //
-    // vdr = new VDR("localhost", 2001, 5000);
-    // }
-    //
-    //
+    private static VDR vdr;
+
+    @BeforeClass
+    public static void setProtocolVersion() {
+        Connection.setVersion(new Version("1.7.0"));
+    }
+
+    @Test
+    public void testGetTimers() throws IOException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTT.class))).thenReturn(new R250(IOUtil.readFile("unittests/lstt.txt")));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        List<Timer> timers = vdr.getTimers();
+        assertEquals(4, timers.size());
+
+        conn = mock(Connection.class);
+        when(conn.send(isA(LSTT.class))).thenReturn(new R550("No timers defined"));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        timers = vdr.getTimers();
+        assertEquals(0, timers.size());
+    }
+
+    @Test
+    public void testGetTimer() throws IOException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTT.class))).thenReturn(new R250(IOUtil.readFile("unittests/lstt.txt")));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        Timer timer = vdr.getTimer(1);
+        assertEquals("Chronik einer Entmietüng", timer.getTitle());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetTimerException() throws IOException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTT.class))).thenReturn(new R550("No timers defined"));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        vdr.getTimer(1);
+    }
+
     // @Test
-    // public void testGetTimers() throws UnknownHostException, IOException {
+    // public void testModifyTimer() throws UnknownHostException, IOException {
     // server.loadTimers("lstt.txt");
-    // List<Timer> timers = vdr.getTimers();
-    // assertEquals(4, timers.size());
     //
-    // server.loadTimers("lstt_no_timers.txt");
-    // timers = vdr.getTimers();
-    // assertEquals(0, timers.size());
-    // }
-    //
-    // @Test
-    // public void testGetTimer() throws UnknownHostException, IOException {
-    // server.loadTimers("lstt.txt");
     // Timer timer = vdr.getTimer(1);
-    // assertEquals("Chronik einer Entmietüng", timer.getTitle());
-    // }
-    //
-    // @Test(expected = RuntimeException.class)
-    // public void testGetTimerException() throws UnknownHostException, IOException {
-    // server.loadTimers("lstt_no_timers.txt");
-    // vdr.getTimer(1);
-    // }
-    //
-    // // @Test
-    // // public void testModifyTimer() throws UnknownHostException, IOException {
-    // // server.loadTimers("lstt.txt");
-    // //
-    // // Timer timer = vdr.getTimer(1);
-    // // timer.setTitle("ABCDEF");
-    // // Response response = vdr.modifyTimer(timer.getID(), timer);
-    // // assertEquals(250, response.getCode());
-    // //
-    // // timer = vdr.getTimer(1);
-    // // assertEquals("ABCDEF", timer.getTitle());
-    // // }
-    //
-    // @Test
-    // public void testDeleteTimer() throws UnknownHostException, IOException {
-    // server.loadTimers("lstt.txt");
-    // Response response = vdr.deleteTimer(1);
+    // timer.setTitle("ABCDEF");
+    // Response response = vdr.modifyTimer(timer.getID(), timer);
     // assertEquals(250, response.getCode());
     //
-    // List<Timer> timers = vdr.getTimers();
-    // assertEquals(3, timers.size());
-    //
-    // response = vdr.deleteTimer(1);
-    // assertEquals(250, response.getCode());
-    //
-    // response = vdr.deleteTimer(2);
-    // assertEquals(250, response.getCode());
+    // timer = vdr.getTimer(1);
+    // assertEquals("ABCDEF", timer.getTitle());
     // }
-    //
-    // @Test
-    // public void testGetRecordings() throws UnknownHostException, IOException {
-    // List<Recording> recordings = vdr.getRecordings();
-    // assertEquals(7, recordings.size());
-    //
-    // server.loadRecordings("lstr_no_recordings.txt");
-    // recordings = vdr.getRecordings();
-    // assertEquals(0, recordings.size());
-    // }
-    //
-    // @Test
-    // public void testGetRecordingDetails() throws UnknownHostException, IOException, ParseException {
-    // server.loadRecordings("lstr.txt");
-    // List<Recording> recordings = vdr.getRecordings();
-    //
-    // Recording rec = vdr.getRecordingDetails(recordings.get(0));
-    // assertEquals("Tagesschau", rec.getTitle());
-    // assertEquals(
-    // "* Bundeskanzlerin Merkel zur Zukunft der schwarz-gelben Koalition * Gewerkschaften fordern höhere Gehälter * Sport: Bundesliga, Basketball-WM und Formel 1",
-    // rec.getDescription());
-    // assertEquals(0, rec.getPriority());
-    // assertEquals(0, rec.getLifetime());
-    // }
-    //
-    // @Test(expected = RuntimeException.class)
-    // public void testGetRecordingDetailsException() throws UnknownHostException, IOException, ParseException {
-    // Recording fake = new Recording();
-    // fake.setNumber(123456);
-    // vdr.getRecordingDetails(fake);
-    // }
-    //
-    // // @Test
-    // // public void testGetEpg() throws UnknownHostException, IOException {
-    // // List<EPGEntry> epg = vdr.getEpg();
-    // // assertEquals(10, epg.size());
-    // // }
-    //
-    // @Test
-    // public void testGetChannels() throws UnknownHostException, IOException, ParseException {
-    // server.loadChannelsConf("channels-mixed.conf");
-    // List<Channel> channels = vdr.getChannels();
-    // assertEquals(57, channels.size());
-    // }
-    //
-    // @Test(expected = RuntimeException.class)
-    // public void testGetChannelsException() throws UnknownHostException, IOException, ParseException {
-    // server.loadChannelsConf("channels-empty.conf");
-    // vdr.getChannels();
-    // }
-    //
-    // @AfterClass
-    // public static void shutdownServer() throws IOException, InterruptedException {
-    // server.shutdown();
-    // }
+
+    @Test
+    public void testDeleteTimer() throws IOException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(DELT.class))).thenReturn(new R250("Timer deleted"));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        Response response = vdr.deleteTimer(1);
+        assertEquals(250, response.getCode());
+    }
+
+    @Test
+    public void testGetRecordings() throws IOException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTR.class))).thenReturn(new R250(IOUtil.readFile("unittests/lstr.txt")));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        List<Recording> recordings = vdr.getRecordings();
+        assertEquals(7, recordings.size());
+
+        conn = mock(Connection.class);
+        when(conn.send(isA(LSTR.class))).thenReturn(new R550("No recordings available"));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        recordings = vdr.getRecordings();
+        assertEquals(0, recordings.size());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetRecordingException() throws IOException, ParseException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTR.class))).thenReturn(new AccessDenied("You are not allowed"));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        vdr.getRecordings();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetRecordingError() throws IOException, ParseException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTR.class))).thenReturn(null);
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        vdr.getRecordings();
+    }
+
+    @Test
+    public void testGetRecordingDetails() throws UnknownHostException, IOException, ParseException {
+        //@formatter:off
+        String recordingData =
+            "C S19.2E-1-1101-28106 Das Erste\n" +
+            "E 30780 1283104800 900 4E FF\n" +
+            "T Test title\n" +
+            "D Test description\n" +
+            "P 23\n" +
+            "L 24\n" +
+            "X 2 03 deu\n" +
+            "X 2 03 2ch\n" +
+            "X 2 05 deu\n" +
+            "V 1283104800\n" +
+            "End of recording information";
+        //@formatter:on
+
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTR.class))).thenReturn(new R215(recordingData));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        Recording rec = createRecording();
+        rec = vdr.getRecordingDetails(rec);
+        assertEquals("Test title", rec.getTitle());
+        assertEquals("Test description", rec.getDescription());
+        assertEquals(23, rec.getPriority());
+        assertEquals(24, rec.getLifetime());
+
+        Calendar vpsTime = Calendar.getInstance();
+        vpsTime.setTimeInMillis(1283104800000L);
+        assertEquals(vpsTime, rec.getVpsTime());
+
+        assertEquals(3, rec.getStreams().size());
+        assertEquals(3, rec.getAudioStreams().size());
+    }
+
+    private Recording createRecording() {
+        Recording rec = new Recording();
+        rec.setChannelID("");
+        rec.setChannelName("");
+        rec.setDescription("Test description");
+        rec.setDisplayTitle("");
+        rec.setDuration(0);
+        rec.setEndTime(0);
+        rec.setEventID(0);
+        rec.setGenres(Collections.<Genre> emptyList());
+        rec.setLifetime(0);
+        rec.setNew(true);
+        rec.setNumber(1);
+        rec.setPriority(0);
+        rec.setShortText("");
+        rec.setStartTime(0);
+        rec.setStreams(Collections.<Stream> emptyList());
+        rec.setTableID(0);
+        rec.setTitle("Test title");
+        rec.setVersion(0);
+        rec.setVpsTime(0);
+        return rec;
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetRecordingDetailsException() throws UnknownHostException, IOException, ParseException {
+        int fakeRecordingNumber = 123456;
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTR.class))).thenReturn(new R550("Recording " + fakeRecordingNumber + " not found"));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        Recording fake = new Recording();
+        fake.setNumber(fakeRecordingNumber);
+        vdr.getRecordingDetails(fake);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetRecordingDetailsError() throws UnknownHostException, IOException, ParseException {
+        int fakeRecordingNumber = 123456;
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTR.class))).thenReturn(null);
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        Recording fake = new Recording();
+        fake.setNumber(fakeRecordingNumber);
+        vdr.getRecordingDetails(fake);
+    }
+
+    @Test
+    public void testGetEpg() throws IOException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTE.class))).thenReturn(new R215(IOUtil.readFile("unittests/lste.txt")));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        List<EPGEntry> epg = vdr.getEpg();
+        assertEquals(18557, epg.size());
+    }
+
+    @Test
+    public void testGetChannels() throws IOException, ParseException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTC.class))).thenReturn(new R250(IOUtil.readFile("unittests/lstc.txt")));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        List<Channel> channels = vdr.getChannels();
+        assertEquals(57, channels.size());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetChannelsException() throws IOException, ParseException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTC.class))).thenReturn(new R550("No channels defined."));
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        vdr.getChannels();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetChannelsError() throws IOException, ParseException {
+        Connection conn = mock(Connection.class);
+        when(conn.send(isA(LSTC.class))).thenReturn(null);
+        vdr = new VDR("localhost", 2001, 5000, conn);
+
+        vdr.getChannels();
+    }
 }
 
