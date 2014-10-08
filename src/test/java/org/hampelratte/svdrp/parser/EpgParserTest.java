c6f9e1a1d7a15ce7540e7a9ca6ac519c3a62091c
Henrik Niehaus <henrik.niehaus@gmx.de>
2014-10-08 19:05:58 +0200

    Changed behaviour of the EPGParser. If now channel name is available, we now return an empty string instead of null

--- src/test/java/org/hampelratte/svdrp/parser/EpgParserTest.java
+++ src/test/java/org/hampelratte/svdrp/parser/EpgParserTest.java
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) Henrik Niehaus
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
@@ -13,7 +13,7 @@
  * 3. Neither the name of the project (Lazy Bones) nor the names of its
  *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -29,7 +29,6 @@
 package org.hampelratte.svdrp.parser;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -79,7 +78,7 @@ public class EpgParserTest {
         first = entries.get(0);
 
         assertEquals("S19.2E-133-5-1793", first.getChannelID());
-        assertNull(first.getChannelName());
+        assertEquals("", first.getChannelName());
     }
 
     @Test
