14b7460de8ae18cdeb3e7844208284524f174cbb
Henrik Niehaus <henrik.niehaus@gmx.de>
2015-01-26 18:22:22 +0100

    Set seconds and milliseconds of the start / end calendars to 0

--- src/main/java/org/hampelratte/svdrp/parsers/TimerParser.java
+++ src/main/java/org/hampelratte/svdrp/parsers/TimerParser.java
@@ -1,19 +1,19 @@
-/* 
+/*
  * Copyright (c) Henrik Niehaus
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 3. Neither the name of the project (Lazy Bones) nor the names of its 
- *    contributors may be used to endorse or promote products derived from this 
+ * 3. Neither the name of the project (Lazy Bones) nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -39,15 +39,15 @@ import org.hampelratte.svdrp.responses.highlevel.Timer;
 
 /**
  * Parses a list of timers received from VDR by the LSTT command
- * 
+ *
  * @author <a href="mailto:hampelratte@users.sf.net">hampelratte@users.sf.net</a>
- * 
+ *
  */
 // FIXME für repeating timer die start und endzeit richtig setzen
 public class TimerParser {
     /**
      * Parses a list of timers received from VDR by the LSTT command
-     * 
+     *
      * @param timerData
      *            A list of timers received from VDR by LSTT command
      * @return A list of Timer objects
@@ -96,6 +96,8 @@ public class TimerParser {
         int minutes = Integer.parseInt(timeString.substring(2));
         time.set(Calendar.HOUR_OF_DAY, hours);
         time.set(Calendar.MINUTE, minutes);
+        time.set(Calendar.SECOND, 0);
+        time.set(Calendar.MILLISECOND, 0);
     }
 
     /**
